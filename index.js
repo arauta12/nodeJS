8fa62eb92d88d1ead6ca53cdd051a2afcf58a71a
arauta12 <andreirauta234@gmail.com>
2026-01-06 12:06:24 -0500

    Fix server close

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log('Server online!');
+});
+
+process.on('SIGINT', () => {
+    server.closeAllConnections();
+    console.log('Closed all connections');
+    process.exit(0);
 });
